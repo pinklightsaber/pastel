f5f78ab7bb013763f87bb34b712c7e7e3eb55df4
pinklightsaber <ctorres@idemar.cl>
2018-07-30 13:55:26 -0400

    barra gris

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 $(function() {
-	console.log('hola');
+
+	 
+    $(window).scroll(function () {
+        var scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 200) {
+            $(".barra").css('background', '#f8f9fa'); //*Transparente
+        } else {
+            $(".barra").css('background', 'transparent'); //*blanco
+        }          
+    });
+
+	
 });
